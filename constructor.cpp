9cf6706e09721535b9a88b2f71f78ef7d165772a
Tanmay Dahake <tanmaydahake19@gmail.com>
2026-09-05 15:27:20 +0530

    Define Basic class with constructor output
    Fix class definition syntax and add constructor message.

---
+++ constructor.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+class Basic()
+{
+public:
+  Basic()
+  {
+    cout<<"constructor is called";
+  }
+      
+};
+int main()
+{
+   cout<<"constructor is called";  
+}
